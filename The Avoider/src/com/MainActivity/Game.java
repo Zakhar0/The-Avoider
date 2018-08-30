639bbe50733f8f2d3c5d7a6b9793901568d519ee
Zakharo <zakravchuk@gmail.com>
2018-08-30 11:05:38 -0400

    First Commit
    Begining

---
+++ The Avoider/src/com/MainActivity/Game.java
@@ -0,0 +1,16 @@
+package com.MainActivity;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
+	
+	public synchronised void start() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
