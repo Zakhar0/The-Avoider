82aaeff40f6477e01342c57df508058122358a05
Zakharo <zakravchuk@gmail.com>
2018-08-30 12:41:58 -0400

    Added Game Loop

--- The Avoider/src/com/MainActivity/Game.java
+++ The Avoider/src/com/MainActivity/Game.java
@@ -7,7 +7,8 @@ public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = -3192454351902361511L;
 	
 	public final static int WIDTH = 640, HEIGHT = WIDTH/12*9;
-
+	private Thread thread;
+	private boolean running = false;
 	
 	public Game() {
 		new Window(WIDTH, HEIGHT, "The Avoider", this);
@@ -15,6 +16,50 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public synchronized void start() {
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void Run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick();
+				delta--;
+			}
+			if (running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis()-timer > 1000) {
+				timer +=1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
+	private void tick() {
+		
+	}
+	private void render() {
 		
 	}
 
