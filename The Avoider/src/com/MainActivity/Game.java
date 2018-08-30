45e2ed4f81a7c845f7f69d8f535e06dad3468ff9
Zakharo <zakravchuk@gmail.com>
2018-08-30 13:07:29 -0400

    Game Loop and  Background Done

--- The Avoider/src/com/MainActivity/Game.java
+++ The Avoider/src/com/MainActivity/Game.java
@@ -1,6 +1,11 @@
 package com.MainActivity;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+
 
 public class Game extends Canvas implements Runnable {
 
@@ -28,7 +33,7 @@ public class Game extends Canvas implements Runnable {
 			e.printStackTrace();
 		}
 	}
-	public void Run() {
+	public void run() {
 		long lastTime = System.nanoTime();
 		double amountOfTicks = 60.0;
 		double ns = 1000000000 / amountOfTicks;
@@ -59,11 +64,21 @@ public class Game extends Canvas implements Runnable {
 	private void tick() {
 		
 	}
+	
 	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs==null) {
+			this.createBufferStrategy(3);
+			return;
+		}
 		
-	}
-
-	public void run() {
+		Graphics g = bs.getDrawGraphics();
+		
+		g.setColor(Color.black);
+		g.fillRect(0,0, WIDTH, HEIGHT);
+		
+		g.dispose();
+		bs.show();
 		
 	}
 	
